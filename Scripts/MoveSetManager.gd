c4ba8004514167d5b266abe685a4f190b37fcb6c
VlaadimirJ <vlad.jeantroux@gmail.com>
2024-01-17 07:39:55 +0100

    Travail final

---
+++ Scripts/MoveSetManager.gd
@@ -0,0 +1,20 @@
+extends Node
+
+#All general moves are stored here
+var generalMoves = {
+	1 : "Straight Punch",
+	2 : "Crouched Punch",
+	3 : "Crouch Idle",
+	4 : "Idle"
+}
+
+var dummyMoves = {
+	1 : "Punc",
+}
+
+
+var nameDictionary = {
+	"Dummy Char" : dummyMoves,
+	
+	"General" : generalMoves
+}
